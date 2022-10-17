18c64970ad3b358575123d37fd2ac76bf96e22bf
shin-shin-programer <shin.shin.event@gmail.com>
2022-10-17 19:33:51 +0900

    Add donate fuction.

--- fundraiser/truffle/contracts/Fundraiser.sol
+++ fundraiser/truffle/contracts/Fundraiser.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.9;
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
 contract Fundraiser is Ownable {
+  struct Donation {
+    uint256 value;
+    uint256 date;
+  }
+  mapping(address => Donation[]) private _donations;
+
   string public name;
   string public url;
   string public imageURL;
@@ -28,5 +34,17 @@ contract Fundraiser is Ownable {
 
   function setBeneficiary(address payable _beneficiary) public onlyOwner {
     beneficiary = _beneficiary;
+  }
+
+  function myDonationsCount() public view returns(uint256) {
+    return _donations[msg.sender].length;
+  }
+
+  function donate() public payable {
+    Donation memory donation = Donation({
+      value: msg.value,
+      date: block.timestamp
+    });
+    _donations[msg.sender].push(donation);
   }
 }
