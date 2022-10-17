92568489e699c1dfdd4dbcd93d90721b5735c0bd
shin-shin-programer <shin.shin.event@gmail.com>
2022-10-17 18:47:26 +0900

    Add attributes to Fundraiser.

--- fundraiser/truffle/contracts/Fundraiser.sol
+++ fundraiser/truffle/contracts/Fundraiser.sol
@@ -3,8 +3,25 @@ pragma solidity ^0.8.9;
 
 contract Fundraiser {
   string public name;
+  string public url;
+  string public imageURL;
+  string public description;
+  address payable public beneficiary;
+  address public custodian;
 
-  constructor(string memory _name) {
+  constructor(
+    string memory _name,
+    string memory _url,
+    string memory _imageURL,
+    string memory _description,
+    address payable _beneficiary,
+    address _custodian
+  ) {
     name = _name;
+    url = _url;
+    imageURL = _imageURL;
+    description = _description;
+    beneficiary = _beneficiary;
+    custodian = _custodian;
   }
 }
--- fundraiser/truffle/test/fundraiser_test.js
+++ fundraiser/truffle/test/fundraiser_test.js
@@ -3,15 +3,52 @@ const FundRaiserContract = artifacts.require('Fundraiser');
 contract('Fundraiser', (accounts) => {
   let fundraiser;
   const name = 'Beneficiary Name';
+  const url = 'beneficiaryname.org';
+  const imageURL = 'https://placekitten.com/600/350';
+  const description = 'Beneficiary description';
+  const beneficiary = accounts[1];
+  const custodian = accounts[0];
 
   describe('initialization', () => {
     beforeEach(async () => {
-      fundraiser = await FundRaiserContract.new(name);
+      fundraiser = await FundRaiserContract.new(
+        name,
+        url,
+        imageURL,
+        description,
+        beneficiary,
+        custodian
+      );
     });
 
     it('gets the beneficiary name', async () => {
       const actual = await fundraiser.name();
       assert.equal(actual, name, 'names should match');
+    });
+
+    it('gets the beneficiary url', async () => {
+      const actual = await fundraiser.url();
+      assert.equal(actual, url, 'url should match');
+    });
+
+    it('gets the beneficiary image url', async () => {
+      const actual = await fundraiser.imageURL();
+      assert.equal(actual, imageURL, 'imageURL should match');
+    });
+
+    it('gets the beneficiary description', async () => {
+      const actual = await fundraiser.description();
+      assert.equal(actual, description, 'description should match');
+    });
+
+    it('gets the beneficiary', async () => {
+      const actual = await fundraiser.beneficiary();
+      assert.equal(actual, beneficiary, 'beneficiary should match');
+    });
+
+    it('gets the custodian', async () => {
+      const actual = await fundraiser.custodian();
+      assert.equal(actual, custodian, 'costodian should match');
     });
   });
 });
