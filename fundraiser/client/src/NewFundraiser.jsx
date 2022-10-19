0b7dad6750c8d0101f66d8921a01c079cdc52ffb
shin-shin-programer <shin.shin.event@gmail.com>
2022-10-19 12:40:43 +0900

    Add NewFoundraiser form layout

--- fundraiser/client/src/NewFundraiser.jsx
+++ fundraiser/client/src/NewFundraiser.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+import { Container, Stack, TextField, Typography, Box } from '@mui/material';
+
 const NewFundraiser = () => {
+  const [name, setFundraiserName] = useState('');
+
   return (
-    <div>
-      <h2>Create a New Fundraiser</h2>
-    </div>
+    <Container maxWidth="sm" sx={{ pt: 5 }}>
+      <Typography variant="h2">Create a New Fundraiser</Typography>
+      <Box variant="form" sx={{ margin: '24px 0' }}>
+        <Stack spacing={3}>
+          <TextField
+            id="outlined-basic"
+            label="Name"
+            variant="outlined"
+            onChange={(e) => setFundraiserName(e.target.value)}
+            value={name}
+          />
+        </Stack>
+      </Box>
+    </Container>
   );
 };
 
