536ba1aa102910679409ebab723865b12fa9676e
shin-shin-programer <shin.shin.event@gmail.com>
2022-10-19 10:27:54 +0900

    Add SimpleStorage Contract to Fundraiser

--- fundraiser/truffle/migrations/1_deploy_fundraiser_factory.js
+++ fundraiser/truffle/migrations/1_deploy_fundraiser_factory.js
@@ -1,5 +1,7 @@
+const SimpleStorage = artifacts.require('SimpleStorage');
 const FundraiserFactoryContract = artifacts.require('FundraiserFactory');
 
 module.exports = function (deployer) {
+  deployer.deploy(SimpleStorage);
   deployer.deploy(FundraiserFactoryContract);
 };
