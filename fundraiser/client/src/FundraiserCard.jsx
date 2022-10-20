35133f808d8281ade3f383d3f1648e3c83f7a880
shin-shin-programer <shin.shin.event@gmail.com>
2022-10-20 14:09:40 +0900

    Add submission form to donate.

--- fundraiser/client/src/FundraiserCard.jsx
+++ fundraiser/client/src/FundraiserCard.jsx
@@ -18,6 +18,12 @@ import {
 import useEth from './contexts/EthContext/useEth';
 
 const FundraiserCard = ({ fundraiser }) => {
+  const {
+    state: { web3, fundraiserArtifact, accounts },
+  } = useEth();
+
+  const [contract, setContract] = useState();
+
   const [state, setState] = useState({
     fundName: '',
     description: '',
@@ -26,15 +32,12 @@ const FundraiserCard = ({ fundraiser }) => {
     url: '',
   });
 
-  const {
-    state: { web3, fundraiserArtifact },
-  } = useEth();
-
   const init = useCallback(
     async (fund) => {
       try {
         const { abi } = fundraiserArtifact;
         const instance = new web3.eth.Contract(abi, fund);
+        setContract(instance);
 
         const fundName = await instance.methods.name().call();
         const description = await instance.methods.description().call();
@@ -58,6 +61,8 @@ const FundraiserCard = ({ fundraiser }) => {
 
   const [open, setOpen] = useState(false);
 
+  const [donateAmount, setDonateAmount] = useState(0);
+
   const handleOpen = useCallback(() => {
     setOpen(true);
   }, []);
@@ -66,11 +71,20 @@ const FundraiserCard = ({ fundraiser }) => {
     setOpen(false);
   }, []);
 
+  const submitFunds = useCallback(async () => {
+    const donation = web3.utils.toWei(donateAmount);
+    await contract.methods.donate().send({
+      from: accounts[0],
+      value: donation,
+      gas: 150000,
+    });
+  }, [web3, donateAmount, contract, accounts]);
+
   useEffect(() => {
     init(fundraiser);
   }, [init, fundraiser]);
 
-  const { fundName, description, imageURL } = state;
+  const { fundName, description, imageURL, totalDonations } = state;
 
   return (
     <div>
@@ -88,6 +102,9 @@ const FundraiserCard = ({ fundraiser }) => {
           <Typography variant="body2" component="p" color="text.secondary">
             {description}
           </Typography>
+          <Typography variant="body2" component="p" color="text.secondary">
+            Total Donations: {totalDonations}
+          </Typography>
         </CardContent>
         <CardActions>
           <Button variant="contained" onClick={handleOpen}>
@@ -102,11 +119,9 @@ const FundraiserCard = ({ fundraiser }) => {
       >
         <DialogTitle id="form-dialog-title">Donate to {fundName}</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            <img src={imageURL} width="200px" height="130px" alt="Fundraiser" />
-            <p>{description}</p>
-          </DialogContentText>
-          <FormControl fullWidth sx={{ mt: 2 }}>
+          <img src={imageURL} width="200px" height="130px" alt="Fundraiser" />
+          <DialogContentText>{description}</DialogContentText>
+          <FormControl fullWidth sx={{ mt: 2, mb: 2 }}>
             <Input
               id="standard-adornment-amount"
               placeholder="0.00"
@@ -115,8 +130,13 @@ const FundraiserCard = ({ fundraiser }) => {
               startAdornment={
                 <InputAdornment position="start">$</InputAdornment>
               }
+              value={donateAmount}
+              onChange={(e) => setDonateAmount(e.target.value)}
             />
           </FormControl>
+          <Button onClick={submitFunds} variant="contained" color="primary">
+            Donate
+          </Button>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
