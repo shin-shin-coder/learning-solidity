ee0b4a2013651debfd10b83298ebc633c2e9f83b
shin-shin-programer <shin.shin.event@gmail.com>
2022-10-21 17:03:21 +0900

    Add a detail of receipt to a Receipts page.

--- fundraiser/client/src/Receipts.jsx
+++ fundraiser/client/src/Receipts.jsx
@@ -1,18 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Container, Typography } from '@mui/material';
 
 const Receipts = () => {
   const location = useLocation();
-  const { state } = location;
+  const [state, setState] = useState();
 
   useEffect(() => {
-    console.log(state);
-  }, [state]);
+    const { donation, date, fund } = location.state;
+    setState({
+      donation,
+      fundName: fund,
+      date: new Date(parseInt(date * 1000)),
+    });
+  }, [location]);
+
+  if (!state) {
+    return null;
+  }
+
+  const { donation, date, fundName } = state;
 
   return (
     <Container maxWidth="md" sx={{ pt: 5 }}>
-      <Typography variant="h1">Receipts</Typography>
+      <Typography variant="h4" sx={{ mb: 2 }}>
+        Thank you for your donation to {fundName}
+      </Typography>
+      <div>
+        <Typography variant="body1" component="p" color="text.primary">
+          Date of Donation: {date.toString()}
+        </Typography>
+        <Typography variant="body1" component="p" color="text.primary">
+          Donation Value: ${donation}
+        </Typography>
+      </div>
     </Container>
   );
 };
