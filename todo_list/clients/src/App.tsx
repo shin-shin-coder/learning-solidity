6c1df7a11717df7495c4f3bab69e2b6d29bb6da1
shin-shin-programer <shin.shin.event@gmail.com>
2022-10-22 19:25:47 +0900

    Add a display of a task count.

--- todo_list/clients/src/App.tsx
+++ todo_list/clients/src/App.tsx
@@ -1,19 +1,35 @@
-import { FC } from 'react';
+import { FC, useEffect, useState, useCallback } from 'react';
 import './App.css';
 import { ethers } from 'ethers';
 import artifact from './contracts/TodoList.json';
 
 const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
+const useTodoList = (contract: ethers.Contract) => {
+  const [count, setCount] = useState<number>();
+
+  const getTaskCount = useCallback(async () => {
+    const res = await contract.functions.taskCount();
+    setCount(res[0].toNumber());
+  }, [contract]);
+
+  useEffect(() => {
+    getTaskCount();
+  }, []);
+
+  return { count };
+};
+
 const App: FC = () => {
   const provider = new ethers.providers.JsonRpcProvider();
   const contract = new ethers.Contract(contractAddress, artifact.abi, provider);
 
-  console.log(contract);
+  const { count } = useTodoList(contract);
 
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="container">
+      <h1>TodoList</h1>
+      <p>タスク件数: {count}件</p>
     </div>
   );
 };
