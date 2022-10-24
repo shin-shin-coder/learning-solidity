7796b1466b1e798359f2c83fa11a3827e1094d54
shin-shin-programer <shin.shin.event@gmail.com>
2022-10-25 01:13:16 +0900

    Add mint nft

--- nft_minter/clients/src/App.tsx
+++ nft_minter/clients/src/App.tsx
@@ -1,12 +1,17 @@
+import { ethers } from 'ethers';
 import { useState, useEffect, useCallback } from 'react';
+import { MyNft__factory } from '../../typechain-types';
 
 interface Window {
   ethereum: any;
 }
 declare var window: Window & typeof globalThis;
 
+const contractAddress = '';
+
 function App() {
   const [currentAccount, setCurrentAccount] = useState('');
+  const [isMinting, setMining] = useState(false);
 
   const checkIfWalletIsConnected = useCallback(async () => {
     const { ethereum } = window;
@@ -50,13 +55,40 @@ function App() {
     }
   }, []);
 
+  const handleClickMint = useCallback(async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = MyNft__factory.connect(contractAddress, provider);
+        const contractWithSigner = contract.connect(signer);
+
+        const nftTx = await contractWithSigner.createNft();
+        console.log('Mining....', nftTx.hash);
+        setMining(true);
+
+        const tx = await nftTx.wait();
+        setMining(false);
+        console.log('Mined!', tx);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log('Error minting character', error);
+    }
+  }, []);
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
 
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div>
+      <h2>Mint your NFT!</h2>
+      <button onClick={handleClickMint}>Mint Character</button>
+      {isMinting && 'Is minting....'}
     </div>
   );
 }
