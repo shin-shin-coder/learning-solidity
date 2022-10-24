891cb7c4df48e3c1c68758a561575ef602b8a8d6
shin-shin-programer <shin.shin.event@gmail.com>
2022-10-24 22:43:59 +0900

    Add contract deploy script.

--- nft_minter/scripts/deploy.ts
+++ nft_minter/scripts/deploy.ts
@@ -1,22 +1,12 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  const MyNft = await ethers.getContractFactory('MyNft');
+  const myNft = await MyNft.deploy();
+  await myNft.deployed();
+  console.log(`MyNft deployed to: ${myNft.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
