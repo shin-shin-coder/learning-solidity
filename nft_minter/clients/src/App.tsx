ec0b64aeced106a5c653360bc23895039666dbdd
shin-shin-programer <shin.shin.event@gmail.com>
2022-10-24 23:32:06 +0900

    Add checkIfWalletIsConnected

--- nft_minter/clients/src/App.tsx
+++ nft_minter/clients/src/App.tsx
@@ -1,4 +1,35 @@
+import { useState, useEffect, useCallback } from 'react';
+
+interface Window {
+  ethereum: any;
+}
+declare var window: Window & typeof globalThis;
+
 function App() {
+  const [currentAccount, setCurrentAccount] = useState('');
+
+  const checkIfWalletIsConnected = useCallback(async () => {
+    const { ethereum } = window;
+    if (ethereum) {
+      console.log('Got the ethereum obejct: ', ethereum);
+    } else {
+      console.log('No Wallet found. Connect Wallet');
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      console.log('Found authorized Account: ', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } else {
+      console.log('No authorized account found');
+    }
+  }, []);
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello world</h1>
