20ae37d502bc24644135e145d87e499148a3ff39
shin-shin-programer <shin.shin.event@gmail.com>
2022-11-01 20:21:01 +0900

    Add an invited limit to InvitationNFT.

--- invitation_nft/contracts/test/InvitationNFT.test.ts
+++ invitation_nft/contracts/test/InvitationNFT.test.ts
@@ -52,5 +52,31 @@ describe('InvitationNFT', () => {
         'Error: caller is not the owner or a member who have nft'
       );
     });
+
+    it('Should mint unsuccessfully when a number of invitation is over limit.', async () => {
+      const { contract, signers } = await loadFixture(deployContract);
+
+      // Setup: Mint to signer1 from owner
+      const signer1 = signers[1];
+      const addr1 = await signer1.getAddress();
+      const tx = await contract.mintAndTransfer(addr1);
+      await tx.wait();
+
+      // Mint to signer2 two times.
+      const contractWithSigner = contract.connect(signer1);
+      const addr2 = await signers[2].getAddress();
+      const tx2 = await contractWithSigner.mintAndTransfer(addr2);
+      await tx2.wait();
+
+      const addr3 = await signers[3].getAddress();
+      const tx3 = await contractWithSigner.mintAndTransfer(addr3);
+      await tx3.wait();
+
+      // Check if exeception is raised on third signer2's mint.
+      const addr4 = await signers[4].getAddress();
+      await expect(
+        contract.connect(signer1).mintAndTransfer(addr4)
+      ).to.be.revertedWith('Error: a number of invitation is over limit');
+    });
   });
 });
